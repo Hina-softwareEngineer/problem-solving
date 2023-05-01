e25100a7e0dea64b345944e1f434190db85a16a8
HINA KHADIM <hinakhadim2002@gmail.com>
2023-05-01 06:16:48 +0000

    feat: Solve Average salary excluding minimum and maximum salary 1-05-2023

---
+++ May/q1.py
@@ -0,0 +1,33 @@
+#### Question : Average Salary Excluding the Minimum and Maximum Salary
+
+## Level : Easy
+## https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+
+import sys
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+
+        minimum = sys.maxsize
+        maximum = -sys.maxsize
+
+        total_sum = 0
+
+        for sal in salary:
+            if sal < minimum:
+                minimum = sal
+            
+            if sal > maximum:
+                maximum = sal
+
+            total_sum += sal
+
+        return (total_sum - minimum - maximum) / len(salary) - 2
+
+
+## Test
+s = Solution()
+print(s.average([4000,3000,1000,2000]))
+print(s.average([1000,2000,3000]))
+
+        
---
+++ template.md
@@ -0,0 +1,14 @@
+#### Question
+
+## Level : Easy
+## link of question
+
+
+```python
+def func_name():
+    pass
+
+
+func_name()
+
+```
